9699d0daab22b3df57ef9b9819b189cc0fc2a775
Paul Malmsten <pmalmsten@gmail.com>
2010-07-10 23:14:32 -0700

    Refactored setup.py to make updating package listing easier

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from distutils.core import setup
 from distutils_extensions import TestCommand, build_py
 
+packages=[
+    'xbee', 
+    'xbee.tests', 
+    'xbee.helpers', 
+    'xbee.helpers.dispatch',
+    'xbee.helpers.dispatch.tests',
+]
+
 setup(
     name='XBee',
     version='1.8.0',
     author='Paul Malmsten',
     author_email='pmalmsten@gmail.com',
-    packages=['xbee', 'xbee.tests'],
+    packages=packages,
     scripts=[],
     url='http://code.google.com/p/python-xbee/',
     license='LICENSE.txt',
     description='Python tools for working with XBee radios',
     long_description=open('README.txt').read(),
     requires=['serial'],
-    provides=['xbee','xbee.tests'],
+    provides=packages,
     cmdclass={'test':TestCommand, 'build_py':build_py}
 )
