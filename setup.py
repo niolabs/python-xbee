25275f2a905e823f4f83064ad8ee662665be4583
Chris Brackert <cbrackert@gmail.com>
2015-10-21 14:44:33 -0700

    Bump version to 2.2.3

--- setup.py
+++ setup.py
@@ -8,22 +8,9 @@ packages=[
     'xbee.helpers.dispatch.tests',
 ]
 
-try:
-    from pypandoc import convert
-    def read_md(f):
-        try:
-            return convert(f, 'rst')
-        except Exception as e:
-            print("warning: pypandoc module not found,"
-                  " not converting Markdown to RST")
-            print(e)
-except ImportError:
-    print("warning: pypandoc module not found, not converting Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
-
 setup(
     name='XBee',
-    version='2.2.2',
+    version='2.2.3',
     author='Paul Malmsten',
     author_email='pmalmsten@gmail.com',
     packages=packages,
@@ -31,7 +18,7 @@ setup(
     url='https://github.com/nioinnovation/python-xbee',
     license='LICENSE.txt',
     description='Python tools for working with XBee radios',
-    long_description=read_md('README.md'),
+    long_description=open('README.rst').read(),
     requires=['serial'],
     provides=packages,
 )
